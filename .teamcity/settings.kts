bd84e7741f433ae17fa5eebfe15f8c3852d772a4
admin <admin@TeamCity>
2021-03-28 18:19:48 +0000

    TeamCity change in 'Spring Petclinic' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
@@ -39,6 +40,17 @@ object Build : BuildType({
 
     vcs {
         root(HttpsGithubComVikloshSpringPetclinicGitRefsHeadsMain)
+    }
+
+    steps {
+        script {
+            name = "create file"
+            scriptContent = """
+                echo "fiel content" > file.ext
+                ls -al
+                export
+            """.trimIndent()
+        }
     }
 
     triggers {
